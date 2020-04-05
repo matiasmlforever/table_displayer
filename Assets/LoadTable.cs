4a1898fe50fd5a2a5f989224b2012dff31df0bc0
Matias Lecaros Aranis <matias.lecaros@etrans.cl>
2020-04-05 18:27:52 -0400

    Deserializing json input

--- Assets/LoadTable.cs
+++ Assets/LoadTable.cs
@@ -1,17 +1,27 @@
-﻿using System.Collections;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LoadTable : MonoBehaviour
 {
     public GameObject tableContainer;
+    public string jsonFilename;
+    public string loadedJson;
+    public JsonLoadedTable jsonLoadedObject;
 
     // Start is called before the first frame update
     void Start() 
     {
         // Add event listeners
-        this.GetComponent<Button>().onClick.AddListener(TaskOnClick);
+        GetComponent<Button>().onClick.AddListener(TaskOnClick);
+
+        // Hardcoding
+        LoadJson(jsonFilename);
     }
 
     // Update is called once per frame
@@ -22,7 +32,20 @@ public class LoadTable : MonoBehaviour
 
     void TaskOnClick()
     {
-        Debug.Log("You have clicked the button!");
+        Debug.Log("Reloading table data...!");
+        LoadJson();
+    }
+
+    private void LoadJson()
+    {
+        LoadJson(jsonFilename);
+    }
+
+    private void LoadJson(string jsonFilename) 
+    {
+        loadedJson = File.ReadAllText(Application.dataPath + "/StreamingAssets/" + jsonFilename);
+        jsonLoadedObject = JsonConvert.DeserializeObject<JsonLoadedTable>(loadedJson);
+
     }
 
 }
---
+++ Assets/Scripts/JsonLoadedTable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonLoadedTable
+{
+
+    private string title;
+    private List<string> columnHeaders;
+    private List<Dictionary<string, string>> data;
+
+    public string Title { get => title; set => title = value; }
+    public List<string> ColumnHeaders { get => columnHeaders; set => columnHeaders = value; }
+    public List<Dictionary<string, string>> Data { get => data; set => data = value; }
+}
