d9db4bcb1a0a6883b31c3481a36a7a633091c212
Matias Lecaros Aranis <matias.lecaros@etrans.cl>
2020-04-05 23:15:25 -0400

    overflowing to avoid scaling problem, and fixed screen size

--- Assets/Scripts/LoadTable.cs
+++ Assets/Scripts/LoadTable.cs
@@ -10,17 +10,52 @@ using UnityEngine.UI;
 public class LoadTable : MonoBehaviour
 {
     public GameObject tableContainer;
-    public GameObject tablePrefab;
+    public GameObject tablePrefab, headColumnPrefab, rowPrefab, rowColumnContentPrefab;
+    private GameObject table;
     public string jsonFilename;
     public string loadedJson;
-    public JsonLoadedTable jsonLoadedObject;
+    private JsonLoadedTable jsonLoadedObject;
 
     // Start is called before the first frame update
     void Start() 
     {
         // Add event listeners
         GetComponent<Button>().onClick.AddListener(TaskOnClick);
+        table = Instantiate(tablePrefab, transform.parent, false);
         LoadJson(jsonFilename);
+        FillTable(jsonLoadedObject, table);
+        Debug.Log("breakpoint");
+
+    }
+
+    private void FillTable(JsonLoadedTable jsonLoadedObject, GameObject table)
+    {
+        GameObject title = table.transform.GetChild(0).gameObject;
+        GameObject head = table.transform.GetChild(1).gameObject;
+
+        //sets title
+        title.GetComponent<Text>().text = jsonLoadedObject.Title;
+
+        //sets headers
+        for (int i = 0; i < jsonLoadedObject.ColumnHeaders.Count; i++)
+        {
+            var temp = Instantiate(headColumnPrefab, head.transform, false);
+            temp.GetComponent<Text>().text = jsonLoadedObject.ColumnHeaders[i];
+        }
+
+        //sets row(s) and its content
+        foreach (var row in jsonLoadedObject.Data)
+        {
+            Debug.Log("fila -   ");
+            var tempRow = Instantiate(rowPrefab, table.transform, false);
+            foreach (KeyValuePair<string,string> item in row)
+            {
+                Debug.Log(item.Key + " - "+ item.Value);
+                var tempRowColunmContent = Instantiate(rowColumnContentPrefab, tempRow.transform, false);
+                tempRowColunmContent.GetComponent<Text>().text = item.Value;
+            }
+        }
+
     }
 
     // Update is called once per frame
